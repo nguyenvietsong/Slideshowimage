6887f8aade9e6e7790570775ac7511e87d854135
nguyenvietsong <55572161+nguyenvietsong@users.noreply.github.com>
2019-09-20 23:19:46 +0700

    Add files via upload

---
+++ slideshow2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace slideshow2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.btnshow);
+            this.panel1.Controls.Add(this.btnnext);
+            this.panel1.Controls.Add(this.btnprev);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(519, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(102, 221);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(75, 23);
+            this.btnprev.TabIndex = 2;
+            this.btnprev.Text = "button2";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(199, 221);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(75, 23);
+            this.btnnext.TabIndex = 3;
+            this.btnnext.Text = "button3";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // btnshow
+            // 
+            this.btnshow.Enabled = false;
+            this.btnshow.Location = new System.Drawing.Point(132, 291);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(75, 23);
+            this.btnshow.TabIndex = 4;
+            this.btnshow.Text = "Slide Show";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 343);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        protected System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ slideshow2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slideshow2
+{
+    public partial class Form1 : Form
+    {
+        string Dirpath;
+
+        int imgindex;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+
+            {
+
+                comboBox1.Items.Add(i);
+
+                comboBox1.SelectedIndex = 0;
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+
+            if (dr != DialogResult.Cancel)
+
+            {
+
+                listBox1.Items.Clear();
+
+                Dirpath = folderBrowserDialog1.SelectedPath;
+
+                string[] files = Directory.GetFiles(Dirpath, "*.Jpg");
+
+                foreach (string file in files)
+
+                {
+    
+                    int pos = file.LastIndexOf("||");
+
+                    string FName = file.Substring(pos + 1);
+
+                    listBox1.Items.Add(FName);
+
+                }
+
+                listBox1.SelectedIndex = imgindex = 0;
+
+                btnprev.Enabled = true;
+
+                btnnext.Enabled = btnshow.Enabled = true;
+
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (btnshow.Text == "Slide Show")
+
+            {
+
+                btnshow.Text = "stop show";
+
+                timer1.Interval = int.Parse(comboBox1.Text) * 1000;
+
+                timer1.Start();
+
+            }
+
+            else
+
+            {
+
+                timer1.Stop();
+
+                btnshow.Text = "Slide Show";
+
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+       //     pictureBox1.ImageLocation = listBox1.SelectedItem.ToString();
+
+             pictureBox1.ImageLocation = Dirpath + "\\" + listBox1.SelectedItem;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (imgindex > 0)
+
+            {
+
+                imgindex -= 1;
+
+                if (imgindex == 0)
+
+                {
+
+                    btnprev.Enabled = false;
+
+                }
+
+                if (imgindex < listBox1.Items.Count - 1)
+
+                    btnnext.Enabled = true;
+
+                listBox1.SelectedIndex = imgindex;
+
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (imgindex < listBox1.Items.Count - 1)
+
+            {
+
+                imgindex += 1;
+
+                if (imgindex == listBox1.Items.Count - 1)
+
+                    btnnext.Enabled = false;
+
+                if (imgindex > 0)
+
+                    btnprev.Enabled = true;
+
+                listBox1.SelectedIndex = imgindex;
+
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            btnnext.PerformClick();
+        }
+    }
+}
